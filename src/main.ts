ea0d60df4cb9edc2d7d41b7f7116eb9253cdce61
aioann01 <andreas.ioannou.gewrgiou@gmail.com>
2026-05-23 23:05:32 +0300

    print port of container

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { appConfiguration } from './appconfig';
 import {ApplicationModule} from "./modules/app.module";
+import {Logger} from "@nestjs/common";
+
+const logger: Logger = new Logger('main.ts');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
@@ -10,6 +14,9 @@ async function bootstrap() {
   app.getHttpServer().keepAliveTimeout = 65000;
   app.getHttpServer().headersTimeout = 66000;
   app.enableShutdownHooks();
-  await app.listen( process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  logger.log("APP LISTENING ON:", port);
+
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
